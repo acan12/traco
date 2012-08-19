f50c94c8039c5ef3d5a83c023106b8d78e537422
arycancer <arycancer@gmail.com>
2012-08-19 18:26:38 +0700

    first commit readme file second

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Traco::Application.config.secret_token = '3c6177579be713f84015993b842e9f0941a8543b52be372c1100c3e4b58291f5239e08a6bf7b3ff917c6aa5de90cde492768e9af197cc3371bdb8b90f0718244'
